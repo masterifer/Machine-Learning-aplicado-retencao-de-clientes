3182e8edd87ec84ad645060ec037c8a03f7cf031
Matheus-Felipe <104593914+masterifer@users.noreply.github.com>
2026-03-09 20:25:18 -0300

    Delete Machine-Learning-aplicado-retencao-de-clientes directory

--- Machine-Learning-aplicado-retencao-de-clientes/src/data/__init__.py
+++
@@ -1,6 +0,0 @@
-
-"""Data loading and schema normalization utilities for churn prediction."""
-
-from .dataset import REQUIRED_TARGET_COLUMN, load_dataset
-
-__all__ = ["REQUIRED_TARGET_COLUMN", "load_dataset"]
